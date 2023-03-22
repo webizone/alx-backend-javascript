f6e339a1f571fe5cf0fafca38c9f298da85d6d54
Chijioke Madubuko <105008387+webizone@users.noreply.github.com>
2023-03-22 20:22:38 +0100

    2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,4 +3,5 @@ export default function handleResponseFromAPI(promise) {
     .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
+}
 
