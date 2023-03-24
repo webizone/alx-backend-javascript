1780b6024bdef1f342f5764347fb4dff2cc98fea
Chijioke Madubuko <105008387+webizone@users.noreply.github.com>
2023-03-24 21:34:58 +0100

    10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const arr = [];
+
+  for (const val of array) {
+    arr.push(`${appendString}${val}`);
+  }
+
+  return arr;
+}
