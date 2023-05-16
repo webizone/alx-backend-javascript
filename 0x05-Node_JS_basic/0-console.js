3b071a9eed0d7b04aab319a990828aa47f71c6e2
Chijioke Madubuko <105008387+webizone@users.noreply.github.com>
2023-05-16 18:24:27 +0100

    0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * displayMessage: displays a message in the console
+ * @message {message to be printed to stdout}
+ */
+const displayMessage = (message) => {
+  console.log(message);
+};
+
+module.exports = displayMessage;
